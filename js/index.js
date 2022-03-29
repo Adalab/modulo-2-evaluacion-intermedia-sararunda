5e5e9a11abccde9113e622988bb8813ee147014d
Sara Fernández Ramos <sararunda@hotmail.com>
2022-03-29 10:57:27 +0200

    add functions

--- js/index.js
+++ js/index.js
@@ -5,12 +5,7 @@ const selectOption = document.querySelector(".js-select");
 const howMuchMoney = document.querySelector(".js-money");
 const winnerGame = document.querySelector(".js-winner");
 const accumulatedMoney = document.querySelector(".js-money-back");
-const one = 1;
-const two = 2;
-const three = 3;
-const four = 4;
-const five = 5;
-const six = 6;
+let saldo = 50;
 //función número azar
 function getRandomNumber(min, max) {
   const randomResult = Math.floor(Math.random() * (max - min + 1) + min);
@@ -21,12 +16,16 @@ function handleClick() {
   //valores en numeros
   const computerOptionNum = getRandomNumber(1, 6);
   const userOptionNum = parseInt(selectOption.value);
-  const selectOptionNum = parseInt(howMuchMoney.value);
+  const amountWagered = parseInt(howMuchMoney.value);
+  console.log(accumulatedMoney.innerHTML);
   if (userOptionNum === computerOptionNum) {
-    winnerGame.innerHTML = "¡Has acertado! :)";
-    accumulatedMoney.innerHTML += selectOptionNum * 2;
+    winnerGame.innerHTML = "¡Has ganado el doble de lo apostado! :)";
+    saldoSum = saldo + amountWagered * 2;
+    accumulatedMoney.innerHTML = saldoSum;
   } else {
-    winnerGame.innerHTML = "¡Has perdido! :(";
+    winnerGame.innerHTML = "¡Has perdido lo apostado! :(";
+    const saldoRest = saldo - amountWagered;
+    accumulatedMoney.innerHTML = saldoRest;
   }
 }
 

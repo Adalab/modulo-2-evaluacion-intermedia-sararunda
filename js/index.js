4a3eb013917642c064d03277cc8961c38e5ad39e
Sara Fernández Ramos <sararunda@hotmail.com>
2022-04-09 09:58:48 +0200

    update issues

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
-"use strict";
+'use strict';
 //variables
-const playBtn = document.querySelector(".js-play");
-const selectOption = document.querySelector(".js-select");
-const howMuchMoney = document.querySelector(".js-money");
-const winnerGame = document.querySelector(".js-winner");
-const accumulatedMoney = document.querySelector(".js-money-back");
+const playBtn = document.querySelector('.js-play');
+const selectOption = document.querySelector('.js-select');
+const howMuchMoney = document.querySelector('.js-money');
+const winnerGame = document.querySelector('.js-winner');
+const accumulatedMoney = document.querySelector('.js-money-back');
+const resetBtn = document.querySelector('.js-reset');
 let saldo = 50;
 //función número azar
 function getRandomNumber(min, max) {
@@ -15,18 +16,23 @@ function getRandomNumber(min, max) {
 function handleClick() {
   //valores en numeros
   const computerOptionNum = getRandomNumber(1, 6);
-  const userOptionNum = parseInt(selectOption.value);
-  const amountWagered = parseInt(howMuchMoney.value);
-  console.log(accumulatedMoney.innerHTML);
+  let userOptionNum = parseInt(selectOption.value);
+  let amountWagered = parseInt(howMuchMoney.value);
   if (userOptionNum === computerOptionNum) {
     winnerGame.innerHTML = `¡Has ganado el doble de lo apostado! <i class="fa-solid fa-face-laugh-beam"></i>`;
     const saldoSum = saldo + amountWagered * 2;
     accumulatedMoney.innerHTML = `Saldo: ${saldoSum}`;
   } else {
     winnerGame.innerHTML = `¡Has perdido lo apostado! <i class="fa-solid fa-face-sad-cry"></i>`;
-    const saldoRest = saldo - amountWagered;
+    const saldoRest = saldoSum - amountWagered;
     accumulatedMoney.innerHTML = `Saldo: ${saldoRest} <i class="fa-solid fa-sack-dollar"></i>`;
   }
 }
 
-playBtn.addEventListener("click", handleClick);
+playBtn.addEventListener('click', handleClick);
+
+function handleClickReset() {
+  howMuchMoney = '';
+}
+resetBtn.addEventListener('click', handleClickReset);
+//reset partida
